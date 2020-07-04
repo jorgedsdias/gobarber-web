4bc20daa71b82b503caac5b8aac7d86a2c035384
Jorge <jorgedsdias@gmail.com>
2020-07-04 13:50:54 -0300

    Estrutura, Context Auth, Toast Messages

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
+
+import AppProvider from './hooks';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AppProvider>
+      <SignIn />
+    </AppProvider>
+
     <GlobalStyle />
   </>
 );
